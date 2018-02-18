0933a2bfd79c499a14ae00dbe96b36d1be8f5338
Niko Viik <Niko.Viik@Wunderdog.fi>
2018-02-18 19:10:39 +0100

    Read battery level from dualshock event data

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dualshock4 controller
+
+This library is used to read _dualshock4_ controller data.
+
+Dualshock4 event data documentation: http://www.psdevwiki.com/ps4/DS4-USB
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ const DUALSHOCK4_VENDOR_ID:u16 = 0x54C;
 const DUALSHOCK4_PRODUCT_ID:u16 = 0x5C4;
 const DUALSHOCK4_BLUETOOTH_RAW_BUFFER_DATA_LENGTH:usize = 10;
 const DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH:usize = 64;
+const DUALSHOCK4_DATA_BLOCK_BATTERY_LEVEL:usize = 12;
+
+
+enum Dualshock4Result {
+    Data(u8),
+    Error(&'static str)
+}
 
 fn get_device_info(api: &HidApi) -> HidDeviceInfo {
     return api.devices()
@@ -13,6 +20,24 @@ fn get_device_info(api: &HidApi) -> HidDeviceInfo {
         .find(|device|
             device.product_id == DUALSHOCK4_PRODUCT_ID && device.vendor_id == DUALSHOCK4_VENDOR_ID)
         .expect("Failed to get device info");
+}
+
+fn read_device_data_to_string(controller: &HidDevice) -> String {
+    let mut buf = [0; DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH];
+    let res = controller.read(&mut buf[..])
+        .expect("Failed to read buffer");
+
+    let mut data_string = String::new();
+
+    for u in &buf[..res] {
+        data_string.push_str(&(u.to_string() + "\t"));
+    }
+
+    return data_string;
+}
+
+fn decode_usb_buf(buf: [u8; DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH]) -> Dualshock4Result {
+    return Dualshock4Result::Data(buf[DUALSHOCK4_DATA_BLOCK_BATTERY_LEVEL]);
 }
 
 fn main() {
@@ -26,17 +51,20 @@ fn main() {
         .expect("Failed to open device");
 
     loop {
+//        let device_data_string = read_device_data_to_string(&controller);
+//        println!("{}", device_data_string);
+
         let mut buf = [0; DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH];
 
-        let res = controller.read(&mut buf[..])
-            .expect("Failed to read buffer");
-
-        let mut data_string = String::new();
-
-        for u in &buf[..res] {
-            data_string.push_str(&(u.to_string() + "\t"));
+        match controller.read(&mut buf[..]) {
+            Ok(DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH) => (),
+            Ok(res) => println!("Error: unexpected data length {}/{}", res, DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH),
+            Err(err) => println!("Error: {:}", err)
         }
 
-        println!("{}", data_string);
+        match decode_usb_buf(buf) {
+            Dualshock4Result::Data(data) => println!("Battery level:\t{:?}", data),
+            Dualshock4Result::Error(err) => println!("Error:\t{}", err)
+        }
     }
 }
