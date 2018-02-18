4a45d8d186ec7b96117c06f067dd91f4c088eb63
Niko Viik <Niko.Viik@Wunderdog.fi>
2018-02-18 18:46:18 +0100

    Print device data as string

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate hidapi;
 
 use hidapi::{HidApi, HidDevice, HidDeviceInfo};
 
-const DUALSHOCK4_VENDOR_ID:u16 = 0x54C; // 1356;
-const DUALSHOCK4_PRODUCT_ID:u16 = 0x5C4; // 1476;
+const DUALSHOCK4_VENDOR_ID:u16 = 0x54C;
+const DUALSHOCK4_PRODUCT_ID:u16 = 0x5C4;
 const DUALSHOCK4_BLUETOOTH_RAW_BUFFER_DATA_LENGTH:usize = 10;
 const DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH:usize = 64;
 
@@ -12,17 +12,31 @@ fn get_device_info(api: &HidApi) -> HidDeviceInfo {
         .into_iter()
         .find(|device|
             device.product_id == DUALSHOCK4_PRODUCT_ID && device.vendor_id == DUALSHOCK4_VENDOR_ID)
-        .expect("Failed to get dualshock device info");
+        .expect("Failed to get device info");
 }
 
 fn main() {
-    let api = HidApi::new().expect("Failed to create HID API instance.");
+    let api = HidApi::new()
+        .expect("Failed to create HID API instance.");
 
     let device_info = get_device_info(&api);
     println!("{:#?}", device_info);
 
-    let controller = api
-        .open(DUALSHOCK4_VENDOR_ID, DUALSHOCK4_PRODUCT_ID)
+    let controller = api.open(DUALSHOCK4_VENDOR_ID, DUALSHOCK4_PRODUCT_ID)
         .expect("Failed to open device");
 
+    loop {
+        let mut buf = [0; DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH];
+
+        let res = controller.read(&mut buf[..])
+            .expect("Failed to read buffer");
+
+        let mut data_string = String::new();
+
+        for u in &buf[..res] {
+            data_string.push_str(&(u.to_string() + "\t"));
+        }
+
+        println!("{}", data_string);
+    }
 }
