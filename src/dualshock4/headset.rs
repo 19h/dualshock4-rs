d47f37334bc25759b986a5333c605bd1b067236e
Niko Viik <Niko.Viik@Wunderdog.fi>
2018-02-19 12:00:07 +0100

    Add get_device function to dualshock4 module

--- src/dualshock4/headset.rs
+++ src/dualshock4/headset.rs
@@ -1,25 +1,26 @@
 use dualshock4::DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH;
 
-const DUALSHOCK4_DATA_BLOCK_HEADSET:usize = 30;
+const DATA_BLOCK_HEADSET:usize = 30;
 
-const DUALSHOCK4_HEADSET_MASK_NONE:u8 = 0x1b;
-const DUALSHOCK4_HEADSET_MASK_HEADPHONES:u8 = 0x3b;
-const DUALSHOCK4_HEADSET_MASK_HEADSET_WITH_MIC:u8 = 0x7b;
+const HEADSET_MASK_NONE:u8 = 0x1b;
+const HEADSET_MASK_HEADPHONES:u8 = 0x3b;
+const HEADSET_MASK_HEADSET_WITH_MIC:u8 = 0x7b;
 
 #[derive(Debug)]
 pub enum Headset {
     None,
     Headphones,
-    HeadsetWithMic
+    HeadsetWithMic,
+    Unknown
 }
 
 pub fn decode(buf: [u8; DUALSHOCK4_USB_RAW_BUFFER_DATA_LENGTH]) -> Headset {
-    let val = buf[DUALSHOCK4_DATA_BLOCK_HEADSET];
+    let val = buf[DATA_BLOCK_HEADSET];
 
     match val {
-        DUALSHOCK4_HEADSET_MASK_NONE => return Headset::None,
-        DUALSHOCK4_HEADSET_MASK_HEADPHONES => return Headset::Headphones,
-        DUALSHOCK4_HEADSET_MASK_HEADSET_WITH_MIC => return Headset::HeadsetWithMic,
-        _ => return Headset::None
+        HEADSET_MASK_NONE => return Headset::None,
+        HEADSET_MASK_HEADPHONES => return Headset::Headphones,
+        HEADSET_MASK_HEADSET_WITH_MIC => return Headset::HeadsetWithMic,
+        _ => return Headset::Unknown
     }
 }
